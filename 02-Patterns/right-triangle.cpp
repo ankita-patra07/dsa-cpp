05cc5d0bc07122638ffbcbbc544b7b21801e89a2
coffeemug990 <patraankita869@gmail.com>
2026-04-19 17:54:29 +0530

    Number Triangle

--- 02-Patterns/right-triangle.cpp
+++ 02-Patterns/right-triangle.cpp
@@ -9,7 +9,7 @@ int main(){
 
     for(int i = 1; i<=n ; i++){
         for (int j = 1; j<=i ; j++){
-            cout << "*";
+            cout << "* ";
         }
         cout << endl;
     }
@@ -17,3 +17,24 @@ int main(){
     
     return 0;
 }
+
+//Number Triangle 
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n ;
+    cout << "Enter a number : ";
+    cin >> n ;
+
+    for(int i = 1; i<=n ; i++){
+        for (int j = 1; j<=i ; j++){
+            cout << j;
+        }
+        cout << endl;
+    }
+    
+    
+    return 0;
+}
+
