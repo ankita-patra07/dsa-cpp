9dce82466b8f808c4b257f4f93828608f6224414
coffeemug990 <patraankita869@gmail.com>
2026-04-21 21:18:55 +0530

    patterns practice

--- 02-Patterns/inverted-pyramid.cpp
+++ 02-Patterns/inverted-pyramid.cpp
@@ -6,9 +6,8 @@ int main(){
     int n = 5 ;
 
     for(int i=n; i>=1; i--){
-
         for(int j=1; j<=n-i; j++){
-            cout<< " ";
+            cout<<" ";
         }
         for(int j=1; j<=i; j++){
             cout<< "* ";
