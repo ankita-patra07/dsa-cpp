19927b0b681ce8d3f56ff0ec669ab94a3f4c0c55
coffeemug990 <patraankita869@gmail.com>
2026-04-23 12:39:23 +0530

    Add sum of digits function

--- 03-Functions/add.cpp
+++ 03-Functions/add.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 
 int add(int a , int b){
+    
     return a + b;
 }
 
---
+++ 03-Functions/sumOfdigits.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int sumOfdigits(int a ){
+    int sum = 0, digit;
+    while(a >0){
+        digit = a % 10;
+        a = a / 10;
+        sum = sum + digit;
+    }
+    return sum;
+}
+int main(){
+    int result= sumOfdigits(123);
+    cout << result;
+}
